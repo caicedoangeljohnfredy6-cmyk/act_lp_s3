9bdd83427a63cc6317f7477f5505674e1c187cae
caicedoangeljohnfredy6-cmyk <caicedoangeljohnfredy6@gmail.com>
2025-10-04 14:16:15 -0500

    Update Ejercicio1.java

--- src/main/java/com/example/Ejercicio1.java
+++ src/main/java/com/example/Ejercicio1.java
@@ -3,6 +3,23 @@ package com.example;
 public class Ejercicio1 {
     public static void ejercicio1() {
         System.out.println("Ejercicio 1");
-        // tu código aquí
-    }
+        int x = 10, y = 5;
+        System.out.println{"---ejercicio 1: operardores Aritmeticos ---"};
+       
+        int resultadoSuma - x + y;
+        System.out.println{"Suma de " + x +" +" + y + " ="+ resultadoSuma};
+       
+        int resultadoResta = x - y;
+        System.out.println{"Resta de "+ x +" = " +" y + " = " + resultadoResta};
+       
+        int resultadoMultiplicacion = x * y;
+        Sytem.out.println{Multiplicacion de " + x + " + y + "resultadoMultiplicacion};
+       
+        int resultadodivision = x / y;
+        System.out.println{Division Entera de " + x + " / " + y + " + resultado Division};
+
+        int resultadoModulo = x % y
+        Systen.out.println{Modulo/Resto de " + x + " % " + y + " = + resultadoModulo
+        
+   }
 }
