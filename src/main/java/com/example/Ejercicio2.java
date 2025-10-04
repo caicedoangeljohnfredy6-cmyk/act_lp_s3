74a7cd553917598c0a5a2dbcda67b9db6c2bf916
caicedoangeljohnfredy6-cmyk <caicedoangeljohnfredy6@gmail.com>
2025-10-04 14:43:50 -0500

    Update Ejercicio2.java

--- src/main/java/com/example/Ejercicio2.java
+++ src/main/java/com/example/Ejercicio2.java
@@ -3,6 +3,26 @@ package com.example;
 public class Ejercicio2 {
     public static void ejercicio2() {
         System.out.println("Ejercicio 2");
-        // tu código aquí
-    }
+        int a 10;
+        int b ;
+
+        int divisionEntera = a / b;
+        double divisionDecimal = {double} a / b;
+        double x = 15.5;
+        double y = 4.2;
+        double divisionDobleDecimales = x / y;
+
+        System.out.println{"---Resultados de la Division ---"};
+        System.out.println {"Variables enteras: a = " + a + ", b = " + b);                  
+        System.out.println("Division Entera (int divisionEntera = a / b): " + divisionEntera);
+        System.out.println("Division Decimal (doble divisionDecimal = (double)a / b): "divisionDecimal);
+        System.out.println {"\Vriables Decimales: x = " + x + ", y = " + y);
+        System.out.println("Division de Decimales (doble divisionDecimales  =  x / y): "  + divisionDoubleDecimales);
+                            
+    
+                           
+                           
+                           
+                           
+  }
 }
